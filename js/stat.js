6388b20e02f34afb0e653f4889b81aeab5e48da9
Анна Левченко <levallevchenko@gmail.com>
2020-01-23 23:29:08 +0300

    Улучшает задание: холст

--- js/stat.js
+++ js/stat.js
@@ -7,20 +7,18 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
-var BAR_GAP = 50;
+var BAR_GAP = 52;
 var TEXT_HEIGHT = 16;
 var CLOUD_COLOR = '#ffffff';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var SELF_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 var TEXT_COLOR = '#000000';
 
-// Отрисовывает облако
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-// Находит максимальный элемент рандомного массива
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 0; i < arr.length; i++) {
@@ -31,45 +29,48 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-// Генерирует случайное число и округляет его до целого
-var getRandomValue = function (max) {
+var getRandomNumber = function (max) {
   return Math.round(Math.random() * max);
 };
 
-// Отрисовывает облако со всей статистикой
+var SHADOW_CLOUD_X = CLOUD_X + CLOUD_GAP;
+var SHADOW_CLOUD_Y = CLOUD_Y + CLOUD_GAP;
+
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, SHADOW_COLOR);
+  renderCloud(ctx, SHADOW_CLOUD_X, SHADOW_CLOUD_Y, SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  // Отрисовывает поздравление
-  ctx.fillStyle = TEXT_COLOR; // Цвет текста
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', CLOUD_X + BAR_GAP, CLOUD_Y + CLOUD_GAP + TEXT_HEIGHT);
-  ctx.fillText('Список результатов', CLOUD_X + BAR_GAP, CLOUD_Y + CLOUD_GAP + TEXT_HEIGHT * 2);
+  var CLOUD_TITLE_X = CLOUD_X + BAR_GAP;
+  var CLOUD_TITLE_Y = CLOUD_Y + CLOUD_GAP + TEXT_HEIGHT;
+  var CONGRETILATION_TITLE = 'Ура, вы победили!';
+  var RESULTS_TITLE = 'Список результатов:';
 
-  // Выбирает максимальное время из массива times
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = '16px PT Mono';
+  ctx.fillText(CONGRETILATION_TITLE, CLOUD_TITLE_X, CLOUD_TITLE_Y);
+  ctx.fillText(RESULTS_TITLE, CLOUD_TITLE_X, CLOUD_TITLE_Y + TEXT_HEIGHT);
+
   var maxTime = getMaxElement(times);
 
-  // Отрисовывает полосы статистики
+
   for (var i = 0; i < players.length; i++) {
-    var time = Math.round(times[i]); // Округляет время игры
+    var time = Math.round(times[i]);
 
-    // x = times[i] * BAR_HEIGHT / MAX_TIME – высота полосы статистики определённого игрока
-    // Отрисовывает счёт игроков
+    var GAMER_BAR_HEIGHT = times[i] * BAR_HEIGHT / maxTime;
+    var BAR_X = CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i;
+    var BAR_Y = CLOUD_HEIGHT - BAR_GAP / 2;
+
     ctx.fillStyle = TEXT_COLOR;
-    ctx.fillText(time, CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_GAP / 2 - TEXT_HEIGHT - times[i] * BAR_HEIGHT / maxTime);
+    ctx.fillText(time, BAR_X, BAR_Y - TEXT_HEIGHT - GAMER_BAR_HEIGHT);
+    ctx.fillText(players[i], BAR_X, BAR_Y);
 
-    // Отрисовывает имена игроков
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_GAP / 2);
-
-    // Выбирает цвет полосы статистики
+    var MAX_SATURATION = 100;
     if (players[i] === 'Вы') {
       ctx.fillStyle = SELF_BAR_COLOR;
     } else {
-      ctx.fillStyle = 'hsl(227,' + getRandomValue(100) + '%, 50%)'; // Случайный синий
+      ctx.fillStyle = 'hsl(227,' + getRandomNumber(MAX_SATURATION) + '%, 50%)';
     }
 
-    // Отрисовывает полосы статистики
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - BAR_GAP / 2 - TEXT_HEIGHT, BAR_WIDTH, -((times[i] * BAR_HEIGHT) / maxTime) + CLOUD_GAP);
+    ctx.fillRect(BAR_X, BAR_Y - TEXT_HEIGHT, BAR_WIDTH, -(GAMER_BAR_HEIGHT) + CLOUD_GAP);
   }
 };
